ccd1c7717e4d5c267e740b3adea1c5878f4f736c
v@nperstar <102234460+vanperstar@users.noreply.github.com>
2022-05-09 13:38:09 +0500

    savol-javob_js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var info = {}
+
+alert("Assalomu aleykum bizning hizmatlar sahifamizga hush kelibsz")
+
+info.fullName = prompt('To\'liq ism familiyangizni kiriting:',)
+info.fullName = prompt("Yoshingizni kiriting:")
+info.fullName = prompt("G'alaba kuni muborak:", "Rahmat")
+info.fullName = prompt("Urushga qatnashganlarni bilaszmi:", "Ha")
+info.fullName = prompt("Bugun ularni tabrikladingizmi:", "Ha" )
+info.fullName = prompt("Ular nechta:", )
+info.fullName = prompt("9-may Xotira va qadirlash kuni muborak:", "Rahmat" )
